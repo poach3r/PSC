d032184be185f8465be5870cbe5c19cc071d6d3a
poacher <58641438+poach3r@users.noreply.github.com>
2023-09-21 18:34:23 -0400

    Add files via upload

--- src/Counter.java
+++ src/Counter.java
@@ -15,6 +15,7 @@ public class Counter extends JFrame
         File file = new File(fileString);
         //scans file and sets encounters
         Scanner reader = new Scanner(file);
+        reader.hasNextLine();
         i = reader.nextInt();
         reader.close();
         
