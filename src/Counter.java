895b09de562c08a5cb8ae9e3dab45360d711b947
poacher <58641438+poach3r@users.noreply.github.com>
2023-09-27 10:24:07 -0400

    1.2
    Fixed a bug that caused PickMethod to fail
    Added an option for Soft Resets
    Refactored code

--- src/Counter.java
+++ src/Counter.java
@@ -8,28 +8,39 @@ import java.io.*;
 public class Counter extends JFrame
     implements ActionListener
 {
-    JFrame f = new JFrame("PSC");
-    public int i = 0; //shiny encounters
-    public int n = 0; //hunting method
-    public void Initial(int shinycharm, int ms, int sw, int ob, String fileString) throws IOException {
+    public static JFrame f = new JFrame("PSC");
+    public static int i = 0; //shiny encounters
+    public static int n = 0; //hunting method
+    public static Font myFont = new Font("Arial", Font.BOLD, 12);
+
+    private static void setDisplay() {
+        Container c = f.getContentPane();
+        c.setBackground(Color.BLACK);  
+        f.setSize(216, 242);
+        f.setLayout(null); 
+        f.setVisible(true);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private static void getReader(String fileString) throws FileNotFoundException {
         File file = new File(fileString);
-        //scans file and sets encounters
         Scanner reader = new Scanner(file);
         reader.hasNextLine();
         i = reader.nextInt();
         reader.close();
-        
-        Container c = f.getContentPane();
-        Font myFont = new Font("Arial", Font.BOLD, 12);
-        c.setBackground(Color.BLACK);  
+    }
 
+    private static JLabel getCount() {
         JLabel count = new JLabel("" + i);
         count.setBounds(0, 0, 200, 52);
         count.setFont(myFont);
         count.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
         count.setForeground(Color.WHITE);
         f.add(count);
+        return count;
+    }
 
+    private static void getPlus(String fileString, JLabel count) {
         JButton plus = new JButton("+");
         plus.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
@@ -45,7 +56,9 @@ public class Counter extends JFrame
         plus.setForeground(Color.WHITE);
         plus.setBackground(Color.BLACK);
         f.add(plus);
+    }
 
+    private static void getMinus(String fileString, JLabel count) {
         JButton minus = new JButton("-");
         minus.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
@@ -61,30 +74,32 @@ public class Counter extends JFrame
         minus.setForeground(Color.WHITE);
         minus.setBackground(Color.BLACK);
         f.add(minus);
+    }
 
+    private static void getEh(int shinyCharm, int ms, int sw, int ob) {
         JButton eh = new JButton("End Hunt");
         eh.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
                 if(ms == 1) {
-                    if(shinycharm == 1)
+                    if(shinyCharm == 1)
                         n = 512;
                     else
                         n = 682;
                 }
                 else if(sw == 1) {
-                    if(shinycharm == 1)
+                    if(shinyCharm == 1)
                         n = 683;
                     else
                         n = 1024;
                 }
                 else if(ob == 1) {
-                    if(shinycharm == 1)
+                    if(shinyCharm == 1)
                         n = 512;
                     else
                         n = 819;
                 }
                 else {
-                    if(shinycharm == 1)
+                    if(shinyCharm == 1)
                         n = 512;
                     else
                         n = 683;
@@ -100,7 +115,6 @@ public class Counter extends JFrame
                     eh.setText("You were at odds!");
             }
         });
-
         eh.setFocusPainted(false);
         eh.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
         eh.setBounds(0, 150, 200, 52);
@@ -108,14 +122,9 @@ public class Counter extends JFrame
         eh.setForeground(Color.WHITE);
         eh.setBackground(Color.BLACK);
         f.add(eh);
-
-        f.setSize(216, 242);
-        f.setLayout(null); 
-        f.setVisible(true);
-        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public void writeFile(int i, String fileString) {
+    public static void writeFile(int i, String fileString) {
         File file = new File(fileString);
         String iString = String.valueOf(i);
         file.delete();
@@ -130,5 +139,14 @@ public class Counter extends JFrame
     }
 
     public void actionPerformed(ActionEvent e) {
+    }
+
+    public static void main(int shinyCharm, int ms, int sw, int ob, int sr, String fileString) throws IOException {
+        getReader(fileString);
+        setDisplay();
+        JLabel count = getCount();
+        getPlus(fileString, count);
+        getMinus(fileString, count);
+        getEh(shinyCharm, ms, sw, ob);
     }
 }
--- src/PickMethod.java
+++ src/PickMethod.java
@@ -6,18 +6,26 @@ import java.awt.event.*;
 public class PickMethod extends JFrame
     implements ActionListener
 {
-    JFrame f = new JFrame("PSC");
-    int shinycharm = 0;
-    public void Initial() {
-        Container c = f.getContentPane();
-        Font myFont = new Font("Arial", Font.BOLD, 12);
-        c.setBackground(Color.BLACK);  
+    public static Font myFont = new Font("Arial", Font.BOLD, 12);
+    public static JFrame f = new JFrame("PSC");
+    public static int shinyCharm = 0;
 
+    private static void setLayout() {
+        Container c = f.getContentPane();
+        c.setBackground(Color.BLACK);  
+        f.setSize(216, 342);
+        f.setLayout(null); 
+        f.setVisible(true);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private static void getMs() {
         JButton ms = new JButton("Masuda Method");
         ms.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
                 f.dispose();
-                new FileShit().Initial(shinycharm, 1, 0, 0);
+                new FileShit();
+                FileShit.main(shinyCharm, 1, 0, 0, 0);
             }
         });
         ms.setFocusPainted(false);
@@ -27,12 +35,15 @@ public class PickMethod extends JFrame
         ms.setForeground(Color.WHITE);
         ms.setBackground(Color.BLACK);
         f.add(ms);
-
+    }
+    
+    private static void getSw() {
         JButton sw = new JButton("Sandwich");
         sw.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 f.dispose();
-                new FileShit().Initial(shinycharm, 0, 1, 0);
+                new FileShit();
+                FileShit.main(shinyCharm, 0, 1, 0, 0);
             }
         });
         sw.setFocusPainted(false);
@@ -42,12 +53,15 @@ public class PickMethod extends JFrame
         sw.setForeground(Color.WHITE);
         sw.setBackground(Color.BLACK);
         f.add(sw);
+    }
 
+    private static void getOb() {
         JButton ob = new JButton("Outbreak");
         ob.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
                 f.dispose();
-                new FileShit().Initial(shinycharm, 0, 0, 1);
+                new FileShit();
+                FileShit.main(shinyCharm, 0, 0, 1, 0);
             }
         });
         ob.setFocusPainted(false);
@@ -57,12 +71,32 @@ public class PickMethod extends JFrame
         ob.setForeground(Color.WHITE);
         ob.setBackground(Color.BLACK);
         f.add(ob);
+    }
 
+    private static void getSr() {
+        JButton sr = new JButton("Soft Reset");
+        sr.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) { 
+                new FileShit();
+                FileShit.main(shinyCharm, 0, 0, 0, 1);
+            }
+        });
+        sr.setFocusPainted(false);
+        sr.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
+        sr.setBounds(0, 200, 200, 52);
+        sr.setFont(myFont);
+        sr.setForeground(Color.WHITE);
+        sr.setBackground(Color.BLACK);
+        f.add(sr);
+    }
+
+    private static void getObsw() {
         JButton obsw = new JButton("Outbreak + Sandwich");
         obsw.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
                 f.dispose();
-                new FileShit().Initial(shinycharm, 0, 0, 0);
+                new FileShit();
+                FileShit.main(shinyCharm, 0, 0, 0, 0);
             }
         });
         obsw.setFocusPainted(false);
@@ -72,28 +106,32 @@ public class PickMethod extends JFrame
         obsw.setForeground(Color.WHITE);
         obsw.setBackground(Color.BLACK);
         f.add(obsw);
+    }
 
+    private static void getSc() {
         JButton sc = new JButton("Shiny Charm");
         sc.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) { 
-                shinycharm = 1;
+                shinyCharm = 1;
             }
         });
         sc.setFocusPainted(false);
         sc.setBorder(new MatteBorder(2, 2, 2, 2, Color.WHITE));
-        sc.setBounds(0, 200, 200, 52);
+        sc.setBounds(0, 250, 200, 52);
         sc.setFont(myFont);
         sc.setForeground(Color.WHITE);
         sc.setBackground(Color.BLACK);
         f.add(sc);
-
-        f.setSize(216, 292);
-        f.setLayout(null); 
-        f.setVisible(true);
-        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
+
     public static void main(String[] args) throws Exception {
-        new PickMethod().Initial();
+        setLayout();
+        getMs();
+        getSw();
+        getOb();
+        getSr();
+        getObsw();
+        getSc();
     }
     @Override
     public void actionPerformed(ActionEvent arg0) {
